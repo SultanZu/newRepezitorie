ba668f90a90847b4f6b8273775b4d8473e530c13
SultanZu <77154232+SultanZu@users.noreply.github.com>
2021-04-10 00:36:49 +0600

    lesson 28

--- lesson28/script/add-ti-cart.js
+++ lesson28/script/add-ti-cart.js
@@ -1,2 +1,4 @@
-let ttl = document.getElementsByClassName('title');
-console.log(ttl[0].innerText)
+// for karzina
+let ttl = document.getElementsByClassName('title')[0].innerText;                                   // zagalovka
+let price = document.getElementsByClassName('price')[0].getElementsByTagName('span')[0].innerText; // cena
+let img = document.getElementsByClassName('opis_cont')[0].getElementsByTagName('img');             // img
