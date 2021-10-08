43dfc74aee6bb850aba602ec12f36f04e3ce1d88
SultanZu <zamatuulusultan@gmail.com>
2021-10-08 03:18:22 +0300

    lesson 57

---
+++ lesson57/scipt.js
@@ -0,0 +1,16 @@
+let num1 = 3;
+let num2 = 2;
+
+let symb = "*";
+
+if (symb === "+") {
+    console.log(num1 + num2);
+} else if (symb === "-") {
+    console.log(num1 - num2);
+} else if (symb === "/") {
+    console.log(num1 / num2);
+} else if (symb === "*") {
+    console.log(num1 * num2);
+} else {
+    console.log("error!")
+};
